c92f496c36c27a7c249cb66defbe6c88b9ce1053
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-11-12 03:16:27 +0300

    0

--- pss04_00/src/main/java/com/apress/pss/terrormovies/controller/LoginController.java
+++ pss04_00/src/main/java/com/apress/pss/terrormovies/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("")
 public class LoginController {
-	@RequestMapping(method = RequestMethod.GET, value = "/custom_login")
+	@RequestMapping(method = RequestMethod.GET, value = "/")
 	public String showLogin(){
 		return "login";
 	}
